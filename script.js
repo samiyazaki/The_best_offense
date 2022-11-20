b71268dd1bebcedd3bc76e3e3c273356aa31e47d
Seiya Miyazaki <smsmiyazaki@gmail.com>
2022-11-20 14:20:11 -0500

    Why won't you commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var generateBtn = document.querySelector("#generate").addEventListener("click", 
 var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var alphaCap = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'];
 var alphaLow = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'];
-var specChar = ['!','@','#','$','%','^','&','*','(',')','_','-','+','=','<','>','?','.',','];
+var specChar = [' ', '!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '|', '{', '}', '~' ];
 
 // Confirms and prompts for the poor schmuck who is trying to avoid getting their password brute forced.
 
@@ -28,8 +28,39 @@ while(confirmLength <= 7 || confirmLength >= 129) {
  var confirmAlphaCap = confirm("Smash that OK button to confirm you want CAPS LOCK to be lit or hit cancel if your 2nd grade teacher whacked you with a ruler for putting CAPS in the wrong spot.");
  var confirmAlphaLow = confirm("Smash that OK button to confirm you want all lower case letters or hit cancel if your 2nd grade teacher whacked you with a ruler for forgetting to capitalize words.");
  var confirmSpecChar = confirm("Smash that OK button to confirm you want special characters in the mix or hit cancel if you forget where to even find them.");
+    while(confirmNumber === false && confirmAlphaCap === false && confirmAlphaLow === false && confirmSpecChar === false) {
+      alert("Ayo! You need to pick at least one or your password will be weaker than King Joffrey's love for Sansa");
+    var confirmNumber = confirm("Smash that OK button to confirm you want them digits or hit cancel if you're scared of numbers like I am.");
+    var confirmAlphaCap = confirm("Smash that OK button to confirm you want CAPS LOCK to be lit or hit cancel if your 2nd grade teacher whacked you with a ruler for putting CAPS in the wrong spot.");
+    var confirmAlphaLow = confirm("Smash that OK button to confirm you want all lower case letters or hit cancel if your 2nd grade teacher whacked you with a ruler for forgetting to capitalize words.");
+    var confirmSpecChar = confirm("Smash that OK button to confirm you want special characters in the mix or hit cancel if you forget where to even find them.");  
+    }
+
+var passwordCharacters = []
+
+if (confirmNumber) {
+   passwordCharacters = passwordCharacters.concat(number) 
 }
-// Write password to the #password input
+if (confirmAlphaCap) {
+    passwordCharacters = passwordCharacters.concat(alphaCap) 
+ }
+ if (confirmAlphaLow) {
+    passwordCharacters = passwordCharacters.concat(alphaLow) 
+ }
+ if (confirmSpecChar) {
+    passwordCharacters = passwordCharacters.concat(specChar) 
+ }
+ console.log(passwordCharacters);
+
+ var randomPassword = ""
+
+ for (var i = 0; i < confirmLength; i++){
+    randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+    console.log(randomPassword)
+ }
+ return randomPassword;
+}
+ // Write password to the #password input
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
