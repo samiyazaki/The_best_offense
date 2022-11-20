e74757fb6e2767c6cc3bfbff2ccb1a374bab684f
Seiya Miyazaki <smsmiyazaki@gmail.com>
2022-11-20 11:41:09 -0500

    I did colon, not semicolon

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate").addEventListener("click", 
 // I would love to figure out how to not write each variable separately, but here are all of the possible characters.
 var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var alphaCap = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'];
-var alphaLow = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']:
+var alphaLow = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'];
 var specChar = ['!','@','#','$','%','^','&','*','(',')','_','-','+','=','<','>','?','.',','];
 // Write password to the #password input
 function writePassword() {
