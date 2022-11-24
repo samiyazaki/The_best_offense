cd08717f8232db7b8727b64880ea90a8f53ad957
Seiya Miyazaki <smsmiyazaki@gmail.com>
2022-11-24 15:34:29 -0500

    Adding messages to ensure it works properly

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ var alphaCap = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D',
 var alphaLow = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
 var specChar = [' ', '!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '|', '{', '}', '~' ];
 
-// Confirms and prompts for password generation
+// Confirms being set to variables for password generation.
 
 var confirmLength = "";
 var confirmNumber;
 var confirmAlphaCap;
 var confirmAlphaLow;
 var confirmSpecChar;
-//When I click the button to generate a password I am presented with a series of prompts for password criteria
+//When I click the button to generate a password I am presented with a series of prompts for password criteria.
 function generatePassword() {
   var confirmLength = prompt(
     "Please select a password length between 8 and 128 characters."
@@ -26,9 +26,10 @@ function generatePassword() {
     alert("Please choose between 8 and 128 characters.");
     var confirmLength = prompt(
       "Please select a password length between 8 and 128 characters."
+// If the criteria are not met, the prompt appears to let the user know.
     );
   }
-  //WHEN prompted for password criteria I select which criteria to include in the password
+  //WHEN prompted for password criteria I select which criteria to include in the password.
   var confirmNumber = confirm(
     "Select OK if you wish to use Numbers or Cancel if you do not."
   );
@@ -41,17 +42,18 @@ function generatePassword() {
   var confirmSpecChar = confirm(
     "Select OK if you wish to use Special Characters or Cancel if you do not."
   );
-  //WHEN asked for character types to include in the password I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+  //WHEN asked for character types to include in the password I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters.
   while (
     confirmNumber === false &&
     confirmAlphaCap === false &&
     confirmAlphaLow === false &&
     confirmSpecChar === false
   ) {
-    alert(
+// If the criteria are not met, the prompt appears to let the user know.
+    alert( 
       "You must select at least one set of special characters to generate a password."
     );
-    //WHEN I answer each prompt my input should be validated and at least one character type should be selected
+    //WHEN I answer each prompt my input should be validated and at least one character type should be selected.
     var confirmNumber = confirm(
       "Select OK if you wish to use Numbers or Cancel if you do not."
     );
@@ -66,38 +68,38 @@ function generatePassword() {
     );
   }
 
-  var passwordCharacters = [];
-
+  var passwordChar = [];
+// This var set to passwordChar takes the confirm results from the user inputs and concatenates it to the string. If you check the console log you will see each character is created and then appended to the existing string. 
   if (confirmNumber) {
-    passwordCharacters = passwordCharacters.concat(number);
+    passwordChar = passwordChar.concat(number);
   }
   if (confirmAlphaCap) {
-    passwordCharacters = passwordCharacters.concat(alphaCap);
+    passwordChar = passwordChar.concat(alphaCap);
   }
   if (confirmAlphaLow) {
-    passwordCharacters = passwordCharacters.concat(alphaLow);
+    passwordChar = passwordChar.concat(alphaLow);
   }
   if (confirmSpecChar) {
-    passwordCharacters = passwordCharacters.concat(specChar);
+    passwordChar = passwordChar.concat(specChar);
   }
-  console.log(passwordCharacters);
-  // WHEN all prompts are answered a password is generated that matches the selected criteria
+  console.log(passwordChar);
+  // WHEN all prompts are answered a password is generated that matches the selected criteria.
   var randomPassword = "";
-
+// This is the for loop that develops the final password using Math.floor and Math.random to randomly select values from the string equal to the length set by the user in the first set of prompts.
   for (var i = 0; i < confirmLength; i++) {
     randomPassword =
       randomPassword +
-      passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+      passwordChar[Math.floor(Math.random() * passwordChar.length)];
     console.log(randomPassword);
   }
   return randomPassword;
 }
-
+// part of the provided code to the developer.
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  //WHEN the password is generated the password is either displayed in an alert or written to the page
+  //WHEN the password is generated the password is either displayed in an alert or written to the page.
 }
 generateBtn.addEventListener("click", writePassword);
